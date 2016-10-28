357bafcee87437385660eca59140668236c74ef1
Vahid Hedayati <vahid.hedayati@gmail.com>
2016-10-28 20:47:57 +0100

    Update Application.groovy

--- grails-app/init/grails/bean/validation/examples/Application.groovy
+++ grails-app/init/grails/bean/validation/examples/Application.groovy
@@ -2,8 +2,28 @@ package grails.bean.validation.examples
 
 import grails.boot.GrailsApp
 import grails.boot.config.GrailsAutoConfiguration
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler
+import security.CustomSecurityEventListener
+import security.CustomSecurityFailureEventListener
+import security.RedirectFailureToRegistration
 
 class Application extends GrailsAutoConfiguration {
+    
+    Closure doWithSpring() {
+        {->
+ redirectFailureHandlerExample(SimpleUrlAuthenticationFailureHandler) {
+        defaultFailureUrl = '/failed'
+    }
+
+    redirectFailureHandler(RedirectFailureToRegistration) {
+        defaultFailureUrl = '/failed'
+        registrationUrl = '/'
+    }
+    customerSecurityEventListener(CustomSecurityEventListener)
+    customSecurityFailureEventListener(CustomSecurityFailureEventListener)
+        }
+        
+    }
     static void main(String[] args) {
         GrailsApp.run(Application, args)
     }
